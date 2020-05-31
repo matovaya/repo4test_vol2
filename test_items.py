922c0489505a1e9376d32a0545f9b5a5bb42bbd9
matovaya <62945293+matovaya@users.noreply.github.com>
2020-05-31 16:45:03 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -2,5 +2,6 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_should_be_add_to_basket_button(browser):
 	browser.get(link)
-	assert browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket"), \
-		"Add to basket button is not available"
+	button = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
+	assert button, "Add to basket button is not available"
+
